430267fe77a78a4e2e37ee3bccb45fbc739f7397
clayton <Clayton7510@gmail.com>
2017-02-07 20:47:42 -0500

    Formatting udpates

--- src/main/java/com/deliveredtechnologies/rulebook/Fact.java
+++ src/main/java/com/deliveredtechnologies/rulebook/Fact.java
@@ -6,32 +6,32 @@ package com.deliveredtechnologies.rulebook;
  * Facts are not immutable; they may be changed by rules and used to derive a result state
  */
 public class Fact<T> {
-    private String _name;
-    private T _value;
+  private String _name;
+  private T _value;
 
-    public Fact(String name, T value) {
-        _name = name;
-        _value = value;
-    }
+  public Fact(String name, T value) {
+    _name = name;
+    _value = value;
+  }
 
-    public Fact (T obj) {
-        _name = obj.toString();
-        _value = obj;
-    }
+  public Fact(T obj) {
+    _name = obj.toString();
+    _value = obj;
+  }
 
-    public String getName() {
-        return _name;
-    }
+  public String getName() {
+    return _name;
+  }
 
-    public void setName(String _name) {
-        this._name = _name;
-    }
+  public void setName(String _name) {
+    this._name = _name;
+  }
 
-    public T getValue() {
-        return _value;
-    }
+  public T getValue() {
+    return _value;
+  }
 
-    public void setValue(T _value) {
-        this._value = _value;
-    }
+  public void setValue(T _value) {
+    this._value = _value;
+  }
 }
