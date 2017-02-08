b24675c34a2eb8f4f9e8d96d7680cd7dee66c1e2
clayton <Clayton7510@gmail.com>
2017-02-07 20:20:44 -0500

    Some style updates; updating JavaDocs, indentation, and whatnot.

--- src/main/java/com/deliveredtechnologies/rulebook/Decision.java
+++ src/main/java/com/deliveredtechnologies/rulebook/Decision.java
@@ -6,21 +6,29 @@ import java.util.function.BiFunction;
  * Created by clong on 2/6/17.
  * A special type of rule that has a return type, which may be different from the type of input (Facts).
  */
-public interface Decision <T, U> extends Rule<T> {
-    /**
-     * @param action the action to be performed
-     * @return the current <code>Rule</code> object
-     */
-    Decision<T, U> then(BiFunction<FactMap<T>, Result<U>, RuleState> action);
+public interface Decision<T, U> extends Rule<T> {
 
-    /**
-     *
-     * @return the result object
-     */
-    U getResult();
+  /**
+   * The then method specifies the action taken if <code>when()</code> evaluates to true.
+   * The execution of this 'then' takes precedence over <code>then(Function)</code> if both exist.
+   *
+   * @param action the action to be performed
+   * @return the current <code>Rule</code> object
+   */
+  Decision<T, U> then(BiFunction<FactMap<T>, Result<U>, RuleState> action);
 
-    /**
-     * @param result the instantiated result; useful for aggregation/chaining of a result
-     */
-    void setResult(Result<U> result);
+  /**
+   * The <code>getResult()</code> method returns the stored result from the Decision
+   * The value may be null.
+   *
+   * @return the result object
+   */
+  U getResult();
+
+  /**
+   * The <code>setResult()</code> method initializes the stored result; useful for aggregation/chaining of a result.
+   *
+   * @param result the instantiated result
+   */
+  void setResult(Result<U> result);
 }
--- src/main/java/com/deliveredtechnologies/rulebook/DecisionBook.java
+++ src/main/java/com/deliveredtechnologies/rulebook/DecisionBook.java
@@ -2,23 +2,38 @@ package com.deliveredtechnologies.rulebook;
 
 /**
  * Created by clong on 2/6/17.
- *
+ * DecisionBook is a type of {@link RuleBook} that stores a return type linked to all the rules in the DecisionBook.
  */
 public abstract class DecisionBook<T, U> extends RuleBook<T> {
-    private Result<U> _result = new Result<U>();
+  private Result<U> _result = new Result<U>();
 
-    public final DecisionBook<T, U> withDeafultResult(U result) {
-        _result.setValue(result);
-        return this;
-    }
+  /**
+   * The withDefaultResult method allows a default result value to be specified.
+   * When using the DSL syntax to chain calls, this method should be the first one specified.
+   *
+   * @param result the initial value of the stored result
+   * @return the current DecisionBook object
+   */
+  public final DecisionBook<T, U> withDeafultResult(U result) {
+    _result.setValue(result);
+    return this;
+  }
 
+  /**
+   * The addRule() method allows a rule to be added to the DecisionBook in the abstract <code>defineRules</code>
+   * method.
+   * @param rule  the Decision rule to be added to the DecisionBook
+   */
+  protected final void addRule(Decision<T, U> rule) {
+    super.addRule(rule);
+  }
 
-    protected final void addRule(Decision<T, U> rule) {
-        super.addRule(rule);
-    }
-
-    public U getResult() {
-        return _result.getValue();
-    }
+  /**
+   * The getResult() method allows the result of the DecisionBook rules execution to be retrieved.
+   * @return the stored result value
+   */
+  public U getResult() {
+    return _result.getValue();
+  }
 
 }
