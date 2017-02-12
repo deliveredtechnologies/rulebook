e663f047b9eb53f43dd5347b37d9c05d6620b295
clayton <Clayton7510@gmail.com>
2017-02-12 15:11:55 -0500

    SNAPSHOT only autodeploy

--- travis/before-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_387a20842b2b_key -iv $encrypted_387a20842b2b_iv -in travis/mavenkey.asc.enc -out travis/mavenkey.asc -d
-gpg --fast-import travis/mavenkey.asc
