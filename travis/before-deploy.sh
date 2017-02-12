20ecc5970c35fe0e0aaa53d4cac72d615add8ded
clayton <Clayton7510@gmail.com>
2017-02-12 14:49:39 -0500

    key import

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_387a20842b2b_key -iv $encrypted_387a20842b2b_iv -in mavenkey.asc.enc -out mavenkey.asc -d
+openssl aes-256-cbc -K $encrypted_387a20842b2b_key -iv $encrypted_387a20842b2b_iv -in travis/mavenkey.asc.enc -out travis/mavenkey.asc -d
 gpg --fast-import travis/mavenkey.asc
