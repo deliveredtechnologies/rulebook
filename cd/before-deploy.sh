f0141c174f0934dd741019935483ecccd200e4c0
clayton <Clayton7510@gmail.com>
2017-02-12 12:14:35 -0500

    display key list for debugging Travis CI deploy

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_35e61015f178_key -iv $encrypted_35e61015f178_iv -in travis_maven_code_signing.asc.enc -out travis_maven_code_signing.asc -d
 gpg --fast-import cd/signingkey.asc
-CD
+gpg --list-secret-keys
