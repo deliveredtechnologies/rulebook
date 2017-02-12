59f9b3ae731a742e5424cc7fc8f7250d16e80691
clayton <Clayton7510@gmail.com>
2017-02-12 14:40:18 -0500

    travis permissions debug

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_387a20842b2b_key -iv $encrypted_387a20842b2b_iv -in travis/mavenkey.asc.enc -out travis/mavenkey.asc -d
-ls travis/
+openssl aes-256-cbc -K $encrypted_387a20842b2b_key -iv $encrypted_387a20842b2b_iv -in mavenkey.asc.enc -out mavenkey.asc -d
