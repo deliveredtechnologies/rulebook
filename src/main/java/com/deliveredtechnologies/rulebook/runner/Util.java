39b5a97ebce14628cd6556bc4e2160661896fb79
clayton <Clayton7510@gmail.com>
2017-02-12 22:29:20 -0500

    util class; convert annotated When method to Predicate for Rules

--- src/main/java/com/deliveredtechnologies/rulebook/runner/Util.java
+++ src/main/java/com/deliveredtechnologies/rulebook/runner/Util.java
@@ -1,10 +1,41 @@
 package com.deliveredtechnologies.rulebook.runner;
 
+import com.deliveredtechnologies.rulebook.FactMap;
+import com.deliveredtechnologies.rulebook.annotation.When;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.function.Predicate;
 
 /**
  * Created by clong on 2/12/17.
  */
 public class Util {
+  private Util() {}
   
+  public static Predicate getWhenMethodAsPredicate(FactMap facts, Object obj) {
+    for (Method method : obj.getClass().getMethods()) {
+      for (Annotation annotation : method.getDeclaredAnnotations()) {
+        if (annotation instanceof When && method.getReturnType() == Boolean.class) {
+          return new Predicate() {
+            @Override
+            public boolean test(Object o) {
+              try {
+                return (Boolean)method.invoke(obj);
+              } catch (InvocationTargetException | IllegalAccessException ex) {
+                return false;
+              }
+            }
+          };
+        }
+      }
+    }
+    return new Predicate() {
+      @Override
+      public boolean test(Object o) {
+        return false;
+      }
+    };
+  }
 }
