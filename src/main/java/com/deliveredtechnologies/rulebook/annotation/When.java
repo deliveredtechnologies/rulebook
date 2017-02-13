795440fb9f77f5cca2f070c5e2c6d2ef72284802
clayton <Clayton7510@gmail.com>
2017-02-12 20:45:29 -0500

    When annotation marks methods as conditions

---
+++ src/main/java/com/deliveredtechnologies/rulebook/annotation/When.java
@@ -0,0 +1,15 @@
+package com.deliveredtechnologies.rulebook.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by clong on 2/12/17.
+ * When marks a method as a condition.
+ */
+public @interface When {
+
+}
