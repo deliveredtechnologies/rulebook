927a4665f56bada9041874f8f7606e7924110fac
clayton <Clayton7510@gmail.com>
2017-04-11 00:00:38 -0400

    added a FactoryBean and a new SpringRuleBook for RuleBook support in Spring

---
+++ rulebook-spring/src/main/java/com/deliveredtechnologies/rulebook/spring/RuleBookFactoryBean.java
@@ -0,0 +1,55 @@
+package com.deliveredtechnologies.rulebook.spring;
+
+import com.deliveredtechnologies.rulebook.lang.RuleBookBuilder;
+import com.deliveredtechnologies.rulebook.model.RuleBook;
+import com.deliveredtechnologies.rulebook.model.rulechain.cor.CoRRuleBook;
+import com.deliveredtechnologies.rulebook.model.runner.RuleBookRunner;
+import org.springframework.beans.factory.FactoryBean;
+
+import java.util.Optional;
+
+/**
+ * Spring FactoryBean for RuleBooks.
+ */
+public class RuleBookFactoryBean implements FactoryBean<RuleBook> {
+
+  private Class<? extends RuleBook> _ruleBookType;
+  private String _package;
+
+  public RuleBookFactoryBean(Class<? extends RuleBook> ruleBookType, String pkg) {
+    _ruleBookType = ruleBookType;
+    _package = pkg;
+  }
+
+  public RuleBookFactoryBean(Class<? extends RuleBook> ruleBookType) {
+    this(ruleBookType, null);
+  }
+
+  public RuleBookFactoryBean(String pkg) {
+    this(null, pkg);
+  }
+
+  @Override
+  public RuleBook getObject() throws Exception {
+    if (_package != null) {
+      if (_ruleBookType != null) {
+        return new RuleBookRunner(RuleBookBuilder.create(_ruleBookType).build(), _package);
+      }
+      return new RuleBookRunner(_package);
+    }
+    if (_ruleBookType != null) {
+      return RuleBookBuilder.create(_ruleBookType).build();
+    }
+    return new CoRRuleBook();
+  }
+
+  @Override
+  public Class<?> getObjectType() {
+    return _ruleBookType;
+  }
+
+  @Override
+  public boolean isSingleton() {
+    return false;
+  }
+}
---
+++ rulebook-spring/src/main/java/com/deliveredtechnologies/rulebook/spring/SpringRuleBook.java
@@ -0,0 +1,52 @@
+package com.deliveredtechnologies.rulebook.spring;
+
+import com.deliveredtechnologies.rulebook.NameValueReferableMap;
+import com.deliveredtechnologies.rulebook.Result;
+import com.deliveredtechnologies.rulebook.model.Rule;
+import com.deliveredtechnologies.rulebook.model.RuleBook;
+import com.deliveredtechnologies.rulebook.model.runner.RuleAdapter;
+
+import java.io.InvalidClassException;
+import java.util.Optional;
+
+/**
+ * RuleBook decorator for Spring that allows Rules to be created from either Spring annotated POJO Rules or
+ * by building Rules.
+ */
+public class SpringRuleBook<T> implements RuleBook<T> {
+
+  private RuleBook<T> _ruleBook;
+
+  public SpringRuleBook(RuleBook<T> ruleBook) {
+    _ruleBook = ruleBook;
+  }
+
+  @Override
+  public void addRule(Rule rule) {
+    _ruleBook.addRule(rule);
+  }
+
+  public void addRule(Object rule) throws InvalidClassException {
+    _ruleBook.addRule(new RuleAdapter(rule));
+  }
+
+  @Override
+  public void run(NameValueReferableMap facts) {
+    _ruleBook.run(facts);
+  }
+
+  @Override
+  public void setDefaultResult(T result) {
+    _ruleBook.setDefaultResult(result);
+  }
+
+  @Override
+  public Optional<Result<T>> getResult() {
+    return _ruleBook.getResult();
+  }
+
+  @Override
+  public boolean hasRules() {
+    return _ruleBook.hasRules();
+  }
+}
