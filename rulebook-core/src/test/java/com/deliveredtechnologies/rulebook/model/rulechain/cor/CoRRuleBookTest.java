e20e8d670e160535583ad28738383fa2bc073c18
clayton <Clayton7510@gmail.com>
2017-04-28 18:41:54 -0400

    tests for issue 54

--- rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/rulechain/cor/CoRRuleBookTest.java
+++ rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/rulechain/cor/CoRRuleBookTest.java
@@ -1,7 +1,19 @@
 package com.deliveredtechnologies.rulebook.model.rulechain.cor;
 
+import com.deliveredtechnologies.rulebook.FactMap;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * Created by clong on 4/6/17.
+ * Tests for {@link CoRRuleBook}.
  */
 public class CoRRuleBookTest {
+  @Test
+  public void rulesDefinedInSubclassAreInvoked() {
+    SubCoRRuleBook ruleBook = new SubCoRRuleBook();
+    ruleBook.setDefaultResult("default");
+    ruleBook.run(new FactMap());
+
+    Assert.assertEquals("Success!", ruleBook.getResult().get().getValue());
+  }
 }
