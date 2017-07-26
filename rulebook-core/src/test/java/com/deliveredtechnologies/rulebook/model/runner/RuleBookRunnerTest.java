88cd23d3bc68c08e8b72092728f89b677fa3ad2b
clayton <Clayton7510@gmail.com>
2017-07-26 19:47:44 -0400

    cleaned up some tests

--- rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunnerTest.java
+++ rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunnerTest.java
@@ -2,6 +2,7 @@ package com.deliveredtechnologies.rulebook.model.runner;
 
 import com.deliveredtechnologies.rulebook.Fact;
 import com.deliveredtechnologies.rulebook.FactMap;
+import com.deliveredtechnologies.rulebook.model.Rule;
 import com.deliveredtechnologies.rulebook.model.RuleBook;
 import net.jodah.concurrentunit.Waiter;
 import org.junit.Assert;
@@ -10,9 +11,7 @@ import org.junit.Test;
 import java.util.concurrent.TimeoutException;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.mock;
 
 /**
  * Tests for {@link RuleBookRunner}.
@@ -47,7 +46,7 @@ public class RuleBookRunnerTest {
     Fact<String> fact2 = new Fact("fact2", "Fact");
     FactMap<String> factMap = new FactMap<>();
 
-    RuleBookRunner ruleBookRunner = spy(new RuleBookRunner("com.deliveredtechnologies.rulebook.runner"));
+    RuleBookRunner ruleBookRunner = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
     factMap.put(fact1);
     factMap.put(fact2);
     ruleBookRunner.run(factMap);
@@ -57,26 +56,35 @@ public class RuleBookRunnerTest {
     Assert.assertEquals("Equivalence, Bitches!", ruleBookRunner.getResult().get().toString());
   }
 
+  @Test(expected = IllegalStateException.class)
+  public void rulesCanNotBeAddedByCallingAddRule() {
+    Rule rule = mock(Rule.class);
+    RuleBookRunner ruleBookRunner = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
+    ruleBookRunner.addRule(rule);
+  }
+
   @Test
   public void ruleBookRunnerIsThreadSafe() throws TimeoutException {
     final Waiter waiter = new Waiter();
-
-    FactMap<String> equalFacts1 = new FactMap<>();
-    FactMap<String> equalFacts2 = new FactMap<>();
-    FactMap<String> unequalFacts1 = new FactMap<>();
-    FactMap<String> unequalFacts2 = new FactMap<>();
 
     RuleBook ruleBook1 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
     RuleBook ruleBook2 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
     RuleBook ruleBook3 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
     RuleBook ruleBook4 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
 
+    FactMap<String> equalFacts1 = new FactMap<>();
     equalFacts1.setValue("fact1", "Fact");
     equalFacts1.setValue("fact2", "Fact");
+
+    FactMap<String> equalFacts2 = new FactMap<>();
     equalFacts2.setValue("fact1", "Factoid");
     equalFacts2.setValue("fact2", "Factoid");
+
+    FactMap<String> unequalFacts1 = new FactMap<>();
     unequalFacts1.setValue("fact1", "Fact");
     unequalFacts1.setValue("fact2", "Factoid");
+
+    FactMap<String> unequalFacts2 = new FactMap<>();
     unequalFacts2.setValue("fact1", "Some");
     unequalFacts2.setValue("fact2", "Value");
 
