d3d62921690b51448adc1d5df84ba37d3f3239cc
clayton <clayton7510@gmail.com>
2017-07-25 23:19:35 -0400

    concurrent unit test to help verify thread safety

--- rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunnerTest.java
+++ rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunnerTest.java
@@ -2,8 +2,12 @@ package com.deliveredtechnologies.rulebook.model.runner;
 
 import com.deliveredtechnologies.rulebook.Fact;
 import com.deliveredtechnologies.rulebook.FactMap;
+import com.deliveredtechnologies.rulebook.model.RuleBook;
+import net.jodah.concurrentunit.Waiter;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.concurrent.TimeoutException;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.spy;
@@ -51,5 +55,67 @@ public class RuleBookRunnerTest {
     Assert.assertEquals("So Factual Too!", fact1.getValue());
     Assert.assertEquals("So Factual!", fact2.getValue());
     Assert.assertEquals("Equivalence, Bitches!", ruleBookRunner.getResult().get().toString());
+  }
+
+  @Test
+  public void ruleBookRunnerIsThreadSafe() throws TimeoutException {
+    final Waiter waiter = new Waiter();
+
+    FactMap<String> equalFacts1 = new FactMap<>();
+    FactMap<String> equalFacts2 = new FactMap<>();
+    FactMap<String> unequalFacts1 = new FactMap<>();
+    FactMap<String> unequalFacts2 = new FactMap<>();
+
+    RuleBook ruleBook1 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
+    RuleBook ruleBook2 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
+    RuleBook ruleBook3 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
+    RuleBook ruleBook4 = new RuleBookRunner("com.deliveredtechnologies.rulebook.runner");
+
+    equalFacts1.setValue("fact1", "Fact");
+    equalFacts1.setValue("fact2", "Fact");
+    equalFacts2.setValue("fact1", "Factoid");
+    equalFacts2.setValue("fact2", "Factoid");
+    unequalFacts1.setValue("fact1", "Fact");
+    unequalFacts1.setValue("fact2", "Factoid");
+    unequalFacts2.setValue("fact1", "Some");
+    unequalFacts2.setValue("fact2", "Value");
+
+    new Thread(() -> {
+      ruleBook1.run(equalFacts1);
+      waiter.assertEquals("So Factual Too!", equalFacts1.getValue("fact1"));
+      waiter.resume();
+      waiter.assertEquals("So Factual!", equalFacts1.getValue("fact2"));
+      waiter.resume();
+      waiter.assertEquals("Equivalence, Bitches!", ruleBook1.getResult().get().toString());
+      waiter.resume();
+    }).start();
+
+    new Thread(() -> {
+      ruleBook2.run(unequalFacts2);
+      waiter.assertEquals("Some", unequalFacts2.getValue("fact1"));
+      waiter.resume();
+      waiter.assertEquals("Value", unequalFacts2.getValue("fact2"));
+      waiter.resume();
+    }).start();
+
+    new Thread(() -> {
+      ruleBook3.run(equalFacts2);
+      waiter.assertEquals("So Factual Too!", equalFacts2.getValue("fact1"));
+      waiter.resume();
+      waiter.assertEquals("So Factual!", equalFacts2.getValue("fact2"));
+      waiter.resume();
+      waiter.assertEquals("Equivalence, Bitches!", ruleBook3.getResult().get().toString());
+      waiter.resume();
+    }).start();
+
+    new Thread(() -> {
+      ruleBook4.run(unequalFacts1);
+      waiter.assertEquals("Fact", unequalFacts1.getValue("fact1"));
+      waiter.resume();
+      waiter.assertEquals("Factoid", unequalFacts1.getValue("fact2"));
+      waiter.resume();
+    }).start();
+
+    waiter.await();
   }
 }
