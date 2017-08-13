e5ce783fb8fd6ab200439a1a44cc72b3dcc784c0
clayton <Clayton7510@gmail.com>
2017-08-13 14:15:21 -0400

    testing travis ci automated maven snapshot upload

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if cat gradle.properties | grep -q SNAPSHOT; then
-  ./gradlew clean build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password=""
+  ./gradlew clean build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password="" -Xcheck -Xtest
 fi
 
