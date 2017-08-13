4385cee4316e73bf6a5221f1912ef53cfc907e49
clayton <Clayton7510@gmail.com>
2017-08-13 14:00:39 -0400

    updated travis ci to deploy snapshots

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if cat gradle.properties | grep -q SNAPSHOT; then
-  ./gradlew uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password=""
+  ./gradlew build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password=""
 fi
 
