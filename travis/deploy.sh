988c54d11ff26f64d48e1d177318f0e1aab60361
clayton <clayton7510@gmail.com>
2017-08-13 17:41:32 -0400

    updated deploy to fail if check fails

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if cat gradle.properties | grep -q SNAPSHOT; then
-  ./gradlew build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password="" -x check -x test -x checkstyleTest -x checkstyleTestHtml -x checkstyleMain -x checkstyleMainHtml
+  ./gradlew build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password=""
 fi
 
