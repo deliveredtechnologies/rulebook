8fdb2d99bbfc0ce558e382154aabbab64637f66f
clayton <Clayton7510@gmail.com>
2017-08-13 14:40:52 -0400

    updated README and travis for develop merge

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if cat gradle.properties | grep -q SNAPSHOT; then
-  ./gradlew clean build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password="" -x check -x test -x checkstyleTest -x checkstyleTestHtml -x checkstyleMain -x checkstyleMainHtml
+  ./gradlew build alljavadoc uploadArchives -PnexusUsername="${OSSRH_USERNAME}" -PnexusPassword="${OSSRH_PASSWORD}" -Psigning.keyId="" -Psigning.password="" -x check -x test -x checkstyleTest -x checkstyleTestHtml -x checkstyleMain -x checkstyleMainHtml
 fi
 
