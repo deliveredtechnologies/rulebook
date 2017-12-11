6a33e9ae9e0fecaa027d6924047e7c3ebc15332b
Clayton Long <clayton.long@agilethought.com>
2017-12-11 14:42:19 -0500

    changed GoldenRule to allow for facts to be filtered, including null values

--- rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/RuleBookAuditorTest.java
+++ rulebook-core/src/test/java/com/deliveredtechnologies/rulebook/model/RuleBookAuditorTest.java
@@ -1,10 +1,15 @@
 package com.deliveredtechnologies.rulebook.model;
 
 import com.deliveredtechnologies.rulebook.FactMap;
+import com.deliveredtechnologies.rulebook.NameValueReferableMap;
+import com.deliveredtechnologies.rulebook.Result;
 import com.deliveredtechnologies.rulebook.lang.RuleBookBuilder;
 import com.deliveredtechnologies.rulebook.lang.RuleBuilder;
+import com.deliveredtechnologies.rulebook.model.rulechain.cor.CoRRuleBook;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.function.BiConsumer;
 
 /**
  * Tests for {@link RuleBookAuditor}.
@@ -30,5 +35,27 @@ public class RuleBookAuditorTest {
     Assert.assertEquals(auditor.getRuleStatus("Rule1"), RuleStatus.EXECUTED);
     Assert.assertEquals(auditor.getRuleStatus("Rule2"), RuleStatus.SKIPPED);
     Assert.assertEquals(auditor.getRuleStatus("Rule3"), RuleStatus.EXECUTED);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void rulesAreStillExecutedWithNullFacts() {
+    Rule rule = new GoldenRule(Object.class);
+    BiConsumer<NameValueReferableMap, Result> action = (facts, result) -> result.setValue("Rule was triggered with status=" + facts.get("status") + " and object=" + facts.get("object"));
+    rule.addAction(action);
+
+    AuditableRule aRule = new AuditableRule(rule, "SimpleRule");
+
+    FactMap facts = new FactMap();
+    facts.setValue("status", 1);
+    facts.setValue("object", null);
+
+    RuleBook ruleBook = new CoRRuleBook();
+
+    RuleBookAuditor auditor = new RuleBookAuditor(ruleBook);
+    auditor.addRule(aRule);
+    ruleBook.run(facts);
+
+    Assert.assertEquals(RuleStatus.EXECUTED, auditor.getRuleStatus("SimpleRule"));
   }
 }
