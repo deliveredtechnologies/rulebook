c22e44d72ddf5fe605f33a48881566e0f7e6b80b
clayton <clayton7510@gmail.com>
2018-03-16 13:59:03 -0400

    resolved dependency errors

--- rulebook-core/src/main/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunner.java
+++ rulebook-core/src/main/java/com/deliveredtechnologies/rulebook/model/runner/RuleBookRunner.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.InvalidPathException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
