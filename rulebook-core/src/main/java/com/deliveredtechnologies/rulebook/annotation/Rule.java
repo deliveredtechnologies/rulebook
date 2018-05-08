2f984d8bf009601d2ab5b77b6add76bef11ec1ef
Clayton Long <clayton.long@agilethought.com>
2018-05-08 17:00:44 -0400

    added RuleChainActionType to Rule annotation

--- rulebook-core/src/main/java/com/deliveredtechnologies/rulebook/annotation/Rule.java
+++ rulebook-core/src/main/java/com/deliveredtechnologies/rulebook/annotation/Rule.java
@@ -1,12 +1,14 @@
 package com.deliveredtechnologies.rulebook.annotation;
 
 import com.deliveredtechnologies.rulebook.Decision;
+import com.deliveredtechnologies.rulebook.model.RuleChainActionType;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static com.deliveredtechnologies.rulebook.model.RuleChainActionType.CONTINUE_ON_FAILURE;
 
 /**
  * Rule defines a class to be injected into a {@link Decision}.
@@ -26,4 +28,10 @@ public @interface Rule {
    * Two rules in the same package with the same order will execute in a non-specified order.
    */
   int order() default 1;
+
+  /**
+   * This specifies the impact that a Rule failure should have on the rule chain.
+   */
+  RuleChainActionType ruleChainAction() default CONTINUE_ON_FAILURE;
 }
+
